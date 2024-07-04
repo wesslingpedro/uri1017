187b6b54d860621d4db8ef06a1ac4559f9dee6fe
wesslingpedro <wesslingpedro@gmail.com>
2024-07-04 20:30:51 -0300

    primeiro commit

---
+++ uri1017/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace testes_ec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+
+            int tempo, velocidade, distancia;
+            double litros;
+
+            tempo = int.Parse(Console.ReadLine());
+            velocidade = int.Parse(Console.ReadLine());
+
+
+            distancia = tempo * velocidade;
+
+            litros = distancia / 12.0;
+
+            Console.WriteLine(litros.ToString("F3", CultureInfo.InvariantCulture));
+
+
+
+
+
+        }
+
+    }
+
+}
